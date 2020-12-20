983b7b6450ca6cdf694dbe582b28c60dd243d9b4
mchuongUIT1852 <chuonghuynh240@gmail.com>
2020-12-20 17:18:27 +0700

    edit edit-account.php button

--- mvc/views/admin/Account/edit-account.php
+++ mvc/views/admin/Account/edit-account.php
@@ -109,7 +109,7 @@
             
            
             <div class="col-lg-12 p-t-20 text-center">
-                <button  type="submit" name="editAccount" value="Update" class="mdl-button mdl-button--raised btn-pink" href="<?php echo URLAdmin.'viewaccount'?>"> Submit
+                <button  type="submit" name="editAccount" value="Update" class="mdl-button mdl-button--raised btn-yellow" href="<?php echo URLAdmin.'viewaccount'?>"> Submit
                </button>
                 <button type="button" class="mdl-button mdl-button--raised btn-default">Cancel</button>
             </div> 
