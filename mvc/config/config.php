b782ae0d728bda0d0bfbee87e104f9ce6c468878
sabols2k <dangducchau2000@gmail.com>
2020-12-20 11:51:21 +0700

    p

--- mvc/config/config.php
+++ mvc/config/config.php
@@ -1,7 +1,7 @@
 <?php 
 
 define('URLAdmin', 'http://localhost:8080/Web_App/Quite_Luxury/admin/');
-define('URL_Layout', 'http://localhost:8080/Web_App/Quite_Luxury/layout/');
+define('URL_Layout', 'http://localhost:8080/Web_App/Quite_Luxury/Home/');
 define('URL', 'http://localhost:8080/Web_App/Quite_Luxury/');
 
 define('imgAccount', './mvc/Assets/admin/img/account/');
