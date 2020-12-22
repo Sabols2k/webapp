7e7c826120742e06f59f06e9b255feef4fdbfca2
sabols2k <dangducchau2000@gmail.com>
2020-12-22 22:43:39 +0700

    booking update session

--- mvc/controllers/Booking.php
+++ mvc/controllers/Booking.php
@@ -17,41 +17,79 @@ class Booking extends Controller{
             'RoomType' => '',
             'Description' => '',
             'NameError' =>'',
+            'MailError' => '',
+            'PhonenumberError' => '', 
+            'CountryError' => '',
+            'DateCheckInOutError' => '',
+            'NumberPeopleError' => '',
+            'RoomTypeError' => '',
+            // 'DescriptionError' => '',
 
         ];
-        
+    
         //Check for post
         if(isset($_POST['bookingform'])) {
             //Sanitize post data
-            die();
+            
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-
+            // $datechekInOut= trim($_POST['booking-date']);
+            $datechekInOut = explode('-',trim($_POST['booking-date']));
             $data['booking'] = [
                 'Name' =>trim($_POST['bookingname']),
                 'Mail' => trim($_POST['booking-email']),
                 'Phonenumber' => trim($_POST['booking-phone']), 
                 'Country' => trim($_POST['booking-country']),
-
-                'DateCheckIn' => trim($_POST['daterangepicker_start']),
-                'DateCheckOut' => trim($_POST['daterangepicker_end']),
-
+                'DateCheckIn' => trim($datechekInOut['2'])."-".trim($datechekInOut['1'])."-".trim($datechekInOut['0']),
+                'DateCheckOut' => trim($datechekInOut['5'])."-".trim($datechekInOut['4'])."-".trim($datechekInOut['3']),
                 'NumberAdult' => trim($_POST['booking-adults']),
                 'NumberChildren' => trim($_POST['booking-children']),
                 'RoomType' => trim($_POST['booking-roomtype']),
-                'Description' => trim($_POST['booking-comments'])
+                'Description' => trim($_POST['booking-comments']),
+                
             
             ];
+            if (empty($data['booking']['Name'])) {
+                $data['booking']['NameError'] = '* Please type your name. Please try again.';
+              
+            }
+            if (empty($data['booking']['Mail'])) {
+                $data['booking']['MailError'] = '* Please type your mail. Please try again.';
+              
+            }
+            if (empty($data['booking']['Phonenumber'])) {
+                $data['booking']['PhonenumberError'] = '* Please type your phone. Please try again.';
+              
+            }
+            if (empty($data['booking']['Country'])) {
+                $data['booking']['CountryError'] = '* Please type your country. Please try again.';
+              
+            }
+            if (empty($data['booking']['DateCheckIn']) || empty($data['booking']['DateCheckOut'])) {
+                $data['booking']['DateCheckInOutError'] = '* Please type your date check in or check out. Please try again.';
+              
+            }else{
+                $data['booking']['DateCheckInOutError']="";
+            }
+            
+            if (empty($data['booking']['NumberAdult']) || empty($data['booking']['NumberChildren'])) {
+                $data['booking']['NumberPeopleError'] = '* Please type your number people. Please try again.';
+              
+            }
+            if (empty($data['booking']['RoomType'])) {
+                $data['booking']['RoomTypeError'] = '* Please type your roomtype. Please try again.';
+              
+            }
+
 
             // print_r($data); die();
-            // if(isset($data['booking']['Name'])){
-            //     $this->createGuestSession($data['booking']);
-            // }
-            // else {
-            //     $data['booking']['NameError'] = 'Please type your name. Please try again.';
-
-            //     // $this->view('layout/Booking-form', $data);
-        
-            // }
+            if(empty($data['booking']['NameError'])){
+                $this->createGuestSession($data['booking']);
+                $data['booking']['NameError']="";
+            }
+            else  {
+                $data['booking']['NameError'] = '* Please type your name. Please try again.';
+                //  $this->view('layout/Booking-form', $data);
+            }
             // print_r($_SESSION['guest']);
             // $model=$this->modeladmin("booking");
             // $model->InsertBooking($data['booking']['bBookingID'],
@@ -61,7 +99,7 @@ class Booking extends Controller{
             //     $data['booking']['bDateCheckOut' ],
             //     $data['booking']['bRoomCount' ]
             // );
-            echo "da booking"; die();
+            // echo "da booking"; die();
           
         } 
         // $data['main'] ="Booking/add-booking";
@@ -72,9 +110,16 @@ class Booking extends Controller{
     function createGuestSession($guest){
         $_SESSION['guest']['name'] =  $guest['Name'] ;
         $_SESSION['guest']['mail'] = $guest['Mail'];
+        $_SESSION['guest']['phone'] = $guest['Phonenumber'];
+        $_SESSION['guest']['country'] = $guest['Country'];
         $_SESSION['guest']['dateCheckIn'] = $guest['DateCheckIn'];
         $_SESSION['guest']['dateCheckOut'] =$guest['DateCheckOut'];
+        $_SESSION['guest']['numberAdult'] = $guest['NumberAdult'];
+        $_SESSION['guest']['numberChildren'] = $guest['NumberChildren'];
+        $_SESSION['guest']['roomtype'] = $guest['RoomType'];
+        $_SESSION['guest']['Decription'] = $guest['Description'];
     }
+   
 
 }
 ?>
--- mvc/views/layout/Booking-form.php
+++ mvc/views/layout/Booking-form.php
@@ -1,7 +1,9 @@
 <?php 
+  // echo "name". $data['booking']['Name'];
   print_r($_SESSION['guest']);
-  print_r($data['booking']);
-  echo "name". $data['booking']['Name'];
+  // print_r($data['booking']);
+  
+  // echo  $data['booking']['NameError'];
 ?>
 <!doctype html>
 <html lang="en">
@@ -54,14 +56,14 @@
               </div>
               <p class="mb30">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia deleniti fuga recusandae perferendis modi voluptate, ad ratione saepe voluptas nam provident reiciendis velit nulla repellendus illo consequuntur amet similique hic.</p>
               <!-- BOOKING FORM -->
-              <form  method="POST" class="booking-form-advanced" id="booking-form" >
+              <form  method="POST" class="booking-form-advanced" >
                 <div class="row">
                   <div class="col-md-6">
                     <div class="form-group">
                       <label>Your Name</label>
                       <input name="bookingname" type="text" class="form-control" placeholder="Your Name">
-                      <span>
-                        <?php echo $data['booking']['NameError'] ?>
+                      <span style="color: red" >
+                           <?php echo  $data['booking']['NameError'] ?>
                       </span>
                     </div>
                   </div>
@@ -69,12 +71,18 @@
                     <div class="form-group">
                       <label>Email Address</label>
                       <input class="form-control" name="booking-email" type="email" placeholder="Your Email Address">
+                      <span style="color: red" >
+                           <?php echo  $data['booking']['MailError'] ?>
+                      </span>      
                     </div>
                   </div>
                   <div class="col-md-6">
                     <div class="form-group">
                       <label>Phone Number</label>
                       <input name="booking-phone" type="text" class="form-control" placeholder="Your Phone Number">
+                      <span style="color: red" >
+                           <?php echo  $data['booking']['PhonenumberError'] ?>
+                      </span>
                     </div>
                   </div>
                   <div class="col-md-6">
@@ -112,6 +120,9 @@
                         <option value="United States of America">United States of America</option>
                         <option value="Vietnam">Vietnam</option>
                       </select>
+                      <span style="color: red" >
+                           <?php echo  $data['booking']['CountryError'] ?>
+                      </span>
                     </div>
                   </div>
                   <div class="col-md-6">
@@ -122,6 +133,9 @@
                         </a>
                       </label>
                       <input type="text" class="datepicker form-control " name="booking-date" readonly="readonly">
+                      <span style="color: red" >
+                           <?php echo  $data['booking']['DateCheckInOutError'] ?>
+                      </span>
                     </div>
                   </div>
                   <div class="col-md-6">
@@ -144,6 +158,7 @@
                             <div class="guests-button">
                               <div class="minus"></div>
                               <input type="text" name="booking-adults" class="booking-guests" value="0">
+                               
                               <div class="plus"></div>
                             </div>
                           </div>
@@ -160,6 +175,9 @@
                             </div>
                           </div>
                         </div>
+                        <span style="color: red" >
+                           <?php echo  $data['booking']['NumberPeopleError'] ?>
+                      </span>
                       </div>
                     </div>
                   </div>
@@ -172,6 +190,9 @@
                         <option value="Double Room" data-subtext="<span class='badge badge-info'>$129 / night</span>">Double Room</option>
                         <option value="Deluxe Room" data-subtext="<span class='badge badge-info'>$89 / night</span>">Deluxe Room</option>
                       </select>
+                      <span style="color: red" >
+                           <?php echo  $data['booking']['RoomTypeError'] ?>
+                      </span>
                     </div>
                   </div>
                   <div class="col-md-12">
@@ -181,7 +202,7 @@
                     </div>
                   </div>
                   <div class="col-md-12">
-                    <button type="submit" name="bookingform" class="btn mt50 float-right">
+                    <button type="submit" name="bookingform" class="btn mt50 float-right" id="bookingform">
                       <i class="fa fa-calendar-check-o" aria-hidden="true"></i>
                       BOOK A ROOM NOW
                     </button>
@@ -270,3 +291,23 @@
     ?>
   </body>
 </html>
+
+<script src="https://smtpjs.com/v3/smtp.js"></script>
+<script>
+  $("#bookingform").click(function(event) {
+    var html = `<h1 style="color: #fff; background: #000;" > xin chao </h1> <br/>`;
+    
+    // email = $("#inputEmail").val();
+        Email.send({
+            Host: "smtp.gmail.com",
+            Username: "henrytranls2k@gmail.com",
+            Password: "Henry1234",
+            SecureToken: "Generate token here",
+            From: "henrytranls2k@gmail.com",
+            To: "dangducchau2000@gmail.com",
+            Subject: "You've booked from Quite Luxury!",
+            Body: html+ "xin chao bạn (khong co button)"
+        })
+      };
+
+</script>
